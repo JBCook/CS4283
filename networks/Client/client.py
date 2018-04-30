dc8b84fce711c281bf38049cb1e23b8aa2d5a01f
Shamita <shamitanagalla@gmail.com>
2018-04-30 14:56:32 -0500

    Added commands to client, error handling

--- networks/Client/client.py
+++ networks/Client/client.py
@@ -5,7 +5,7 @@ import pygame
 
 HOST = 'localhost'    
 PORT = 9006
-BUFFER =1024
+BUFFER = 1024
 
 queue = []
 
@@ -18,61 +18,81 @@ def python_client():
     pygame.mixer.init()
     curr_song = ""
     
-    while (1):
-        if serv == False:
+    while 1:
+        if serv is False:
             msg = input("<YOU>: ")
             s.send(msg.encode('utf-8'))
 
         rec_msg = s.recv(BUFFER)
         rec_msg = rec_msg.decode('utf-8')
-        print("<" + HOST +">: " + rec_msg)
-        if (rec_msg == "What File?"):
+        print("<" + HOST + ">: " + rec_msg)
+        if rec_msg == "What File?":
             file(s)
-            s.send(str(queue).encode());
+            s.send(str(queue).encode())
             rec_msg = s.recv(BUFFER)
             rec_msg = rec_msg.decode('utf-8')
-            print("<" + HOST +">: " + rec_msg)
-        if (rec_msg == "PLAY SONG"):
+            print("<" + HOST + ">: " + rec_msg)
+        if rec_msg == "PLAY SONG":
             serv = True
             curr_song = queue.pop()
-            pygame.mixer.music.load (curr_song)
+            pygame.mixer.music.load(curr_song)
             pygame.mixer.music.play()     
-            #s.send((curr_song+ " is playing").encode());
-        if (rec_msg == "PAUSE SONG"):
+            # s.send((curr_song+ " is playing").encode());
+        if rec_msg == "PAUSE SONG":
             pygame.mixer.music.pause()
             serv = True
-            #s.send("Music Paused".encode())
-        if (rec_msg == "RESUME SONG"):
+            # s.send("Music Paused".encode())
+        if rec_msg == "RESUME SONG":
             serv = True
             pygame.mixer.music.unpause()
             s.send("Music Resumed".encode())
-        if (rec_msg == "NEXT SONG"):
-            #serv = True
-            if(queue != []):
+        if rec_msg == "NEXT SONG":
+            # serv = True
+            if queue != []:
                 curr_song = queue.pop()
                 pygame.mixer.music.load (curr_song)
                 pygame.mixer.music.play()
-                #s.send((curr_song + " is now playing").encode())
+                # s.send((curr_song + " is now playing").encode())
             else:
                 s.send("Playlist is Empty. Load More Songs".encode())
                 serv = True
+        if rec_msg == "REWIND SONG":
+            pygame.mixer.music.rewind()
+            serv = True
+        if rec_msg == "INCREASE VOLUME":
+            vol = pygame.mixer.music.get_volume()
+            if vol < 1:
+                vol = vol + 0.1
+            pygame.mixer.music.set_volume(vol)
+            serv = True
+        if rec_msg == "DECREASE VOLUME":
+            vol = pygame.mixer.music.get_volume()
+            if vol > 0:
+                vol = vol - 0.1
+            pygame.mixer.music.set_volume(vol)
+            serv = True
         else:
             serv = False
+
+
 def file(s):
     name = input("<YOU>: ")
     s.send(name.encode('utf-8'))
-    with open('new_' +name, 'wb') as f:
-        print("ALERT...writing to file..")
-        while True:
-            data = s.recv(BUFFER)
-            if (data[-3:] == "END".encode('utf-8')):
-                print("ALERT...finished writing to file..")
-                queue.append('new_'+name)
-                break
-            if not data:
-                break
-            f.write(data)
-        f.close()
+    try:
+        with open('new_' + name, 'wb') as f:
+            print("ALERT...writing to file..")
+            while True:
+                data = s.recv(BUFFER)
+                if data[-3:] == "END".encode('utf-8'):
+                    print("ALERT...finished writing to file..")
+                    queue.append('new_'+name)
+                    break
+                if not data:
+                    break
+                f.write(data)
+            f.close()
+    except EnvironmentError as e:
+        print("Error: " + e)
     return
 
 
--- networks/Server/server.py
+++ networks/Server/server.py
@@ -5,32 +5,39 @@ HOST = 'localhost'
 PORT = 9006
 BUFFER = 1024
 
+
 def python_server():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind((HOST, PORT))
     s.listen(10)
-    conn= None
-    while (1):
+    conn = None
+    while 1:
         if conn is None:
             conn, addr = s.accept()
         msg = conn.recv(BUFFER)
         msg = msg.decode('utf-8')
-        print("<", s.getsockname(), ">: " , msg)
-        if (msg == "File Request"):
+        print("<", s.getsockname(), ">: ", msg)
+        if msg == "File Request":
             conn.send("What File?".encode('utf-8'))
-            file(conn,s)
+            file(conn, s)
         else:
             msg = input("<YOU>: ")
             conn.send(msg.encode('utf-8'))
 
-def file(c,s):
+
+def file(c, s):
     filename = c.recv(BUFFER)
     filename = filename.decode('utf-8')
-    print("<", s.getsockname(), ">: " , filename)
-    with open(filename, 'rb') as f:
-        for data in f:
-            c.sendall(data)
-        c.send("END".encode('utf-8'))
-    
+    print("<", s.getsockname(), ">: ", filename)
+    try:
+        with open(filename, 'rb') as f:
+            for data in f:
+                c.sendall(data)
+            c.send("END".encode('utf-8'))
+    except EnvironmentError:
+        c.send("File not found.".encode('utf-8'))
+
+
 if __name__ == "__main__":
     sys.exit(python_server())
+
